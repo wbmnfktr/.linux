33c4cc45d9b43b8405be0eb057bed9fa69550e46
wbmnfktr <wbmnfktr@webmanufaktur.net>
2022-04-26 23:53:22 +0200

    Fixed a few installer issues, added --noconfirm

--- setup.sh
+++ setup.sh
@@ -16,24 +16,24 @@ sudo pacman -Syyu
 # Nost important
 # SSH
 echo "Installing SSH Server"
-sudo pacman -S openssh
+sudo pacman -S --noconfirm openssh
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
 
 # Base-devel needed for more advanced setups
 echo "Installing base-devel"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ base-devel
 
 # Multitools for managing dotfiles and others
 echo "Installing git and stow"
-sudo pacman -S 
+sudo pacman -S --noconfirm
   \ stow
 
 # Installing all other needed DEV tools
 # Neovim
 echo "Installing DEV Tools"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ git
   \ neovim
   \ python-pynvim
@@ -42,21 +42,26 @@ sudo pacman -S
 
 # Qutebrowser
 echo "Installing Qutebrowser and Adblocker"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ qutebrowser
   \ python-adblock
 
 # Libre Office, Thunderbird
 echo "Installing Office Suite"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ libreoffice-fresh
 # Filezilla
 echo "Installing FTP/SFTP Client"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ filezilla
 
 # Firewall ufw gufw
 echo "Installing Firewall"
-sudo pacman -S
+sudo pacman -S --noconfirm
   \ ufw
   \ gufw
+
+# Installing NEOFETCH
+echo "Installing NEOFETCH"
+sudo pacman -S --noconfirm
+  \ neofetch
